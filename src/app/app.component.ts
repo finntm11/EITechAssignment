a8a85260717f90c972a85295439508b2329f8263
Finn Tomasula Martin <fmartsula@gmail.com>
2025-03-01 12:07:42 -0700

    get data poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CryptoService } from './services/crypto.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'EITechAssignment';
+export class AppComponent implements OnInit {
+
+  cryptoData: any[] = [];
+
+  constructor(private cryptoService: CryptoService) { }
+
+  ngOnInit(): void {
+    this.cryptoService.getCryptoData().subscribe({
+      next: (data) => {
+        this.cryptoData = data.data;  // CoinCap returns data under a "data" key
+      },
+      error: (err) => {
+        console.error('Error fetching crypto data', err);
+      }
+    });
+  }
+
 }
---
+++ src/app/services/crypto.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CryptoService {
+
+  private apiUrl = 'https://api.coincap.io/v2/assets';  // CoinCap API URL
+
+  constructor(private http: HttpClient) { }
+
+  getCryptoData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+}
